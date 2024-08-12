77fb8661fe43ac195586f92693db6b06f6e1d87f
Reactor46 <jbattista1432@gmail.com>
2024-08-12 08:06:53 -0700

    Scripting Templates

---
+++ SCRIPTING_TEMPLATES/failover-recovery-dns.cmd
@@ -0,0 +1,19 @@
+cls
+
+nslookup creditone.com 8.8.8.8 | find "Address"
+
+nslookup creditonebank.com 8.8.8.8 | find "Address"
+
+nslookup chat.creditonebank.com 8.8.8.8 | find "Address"
+
+nslookup images.creditonebank.com 8.8.8.8 | find "Address"
+
+nslookup prequal.creditonebank.com 8.8.8.8 | find "Address"
+
+nslookup accept.creditonebank.com 4.2.2.2 | find "Address"
+
+nslookup m.creditonebank.com 8.8.8.8 | find "Address"
+
+
+
+
